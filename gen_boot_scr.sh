2470caa3a1dd97f95b313a76c4d0a8d089a4c0af
Apaczer <94932128+Apaczer@users.noreply.github.com>
2022-12-20 20:21:56 +0100

    add `q90_v2` variant
    - add variant with miyoo_kbd.miyoo_ver == 7 thus swapped from D-PAD to FaceButtons hotkeys
    - Rfunction button behaves as R3 (or L3 with hotkey press)

--- gen_boot_scr.sh
+++ gen_boot_scr.sh
@@ -5,6 +5,7 @@ mkimage -C none -A arm -T script -d boot-bb2x.cmd boot/variants/bittboy2x_v2/boo
 mkimage -C none -A arm -T script -d boot-bb35.cmd boot/variants/bittboy3.5/boot.scr
 mkimage -C none -A arm -T script -d boot-pocketgo-v90-q90.cmd boot/variants/pocketgo_TE/boot.scr
 mkimage -C none -A arm -T script -d boot-pocketgo-v90-q90.cmd boot/variants/v90_q90/boot.scr
+mkimage -C none -A arm -T script -d boot-q90_v2.cmd boot/variants/q90_v2/boot.scr
 mkimage -C none -A arm -T script -d boot-v90_v2.cmd boot/variants/v90_v2/boot.scr
 mkimage -C none -A arm -T script -d boot-q20.cmd boot/variants/q20/boot.scr
 mkimage -C none -A arm -T script -d boot-m3.cmd boot/variants/m3/boot.scr
